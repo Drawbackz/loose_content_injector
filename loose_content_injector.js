4dd7c7dedf81b007bedd41acb01c1d572da6a35f
Drawbackz <TOCTRTU2@gmail.com>
2017-05-29 17:18:04 -0500

    Functionality Update
    Cancel injection
    Remove Content Item
    Error information
    Inject callback

--- loose_content_injector.js
+++ loose_content_injector.js
@@ -1,32 +1,51 @@
 function LooseContentInjector(injectorName, injectionInterval_ms) {
 
-    var _contentItems = [];
+    var instance = this;
 
+    var _contentItems = [];
     var _injectCallback = null;
 
+    var canceled = false;
+
+    var hasError = false;
+    var errorMessage = null;
+
     this.inject = function(callback) {
+        _injectCallback = callback;
         console.log('LCI [' + injectorName + ']: Injecting...');
         injectLoop();
+    };
+    this.cancelInjection = function(){
+        canceled = true;
+    };
+
+    this.hasError = function(){
+        return hasError;
+    };
+    this.getError = function(){
+        return errorMessage;
     };
 
     this.ContentItem = function() {
 
         var instance = this;
 
+        this.required = false;
+
         this.name = null;
         this.inject = null;
         this.callback = null;
-        this.canLoadCheck = null;
-        this.isLoadedCheck = null;
+        this.canInject = null;
+        this.isInjected = null;
 
         this.failures = 0;
         this.failureLimit = 250;
         this.lastError = null;
 
         this.isFailed = function(){ return instance.failures >= instance.failureLimit; };
-        this.isLoaded = function(){
+        this.canInject = function(){
             try{
-                return instance.isLoadedCheck();
+                return instance.canInject();
             }
             catch(e) {
                 instance.lastError = e;
@@ -34,32 +53,82 @@ function LooseContentInjector(injectorName, injectionInterval_ms) {
             }
         };
         this.isCompleted = function () {
-            return instance.isFailed() || instance.isLoaded();
+            return instance.isFailed() || instance.isInjected();
+        };
+        this.reset = function(){
+
+            instance.failures = 0;
+            instance.failureLimit = 250;
+            instance.lastError = null;
+
         };
 
     };
-
     this.addContentItem = function(contentItem){
         _contentItems.push(contentItem);
     };
+    this.removeContentItem = function(contentItem){
+        var index;
+        if((index = _contentItems.indexOf(contentItem)) > -1){
+            _contentItems.splice(index, 1);
+        }
+    };
 
-    function isComplete() {
+    this.isCanceled = function(){
+        return canceled;
+    };
+
+    this.isComplete = function() {
+        if(instance.isCanceled() || instance.error){
+            return true;
+        }
         for(var i = 0; i < _contentItems.length; i++){
             if(!_contentItems[i].isCompleted()){
                 return false;
             }
         }
         return true;
-    }
+    };
 
-    function sendInjectCallback() {
+    this.reset = function(){
+        for(var i = 0; i < _contentItems.length; i++){
+            _contentItems[i].reset();
+        }
+    };
+
+    function sendInjectCallback(success) {
         if (typeof(_injectCallback) === 'function') {
-            _injectCallback();
+            _injectCallback(success);
         }
     }
     function sendContentCallback(contentItem) {
         if (typeof(contentItem.callback) === 'function') {
             contentItem.callback(contentItem);
+        }
+    }
+    function sendCompleted(){
+
+        var loadedCount = 0;
+
+        for(var i = 0; i < _contentItems.length; i++){
+            if(_contentItems[i].isInjected()){
+                loadedCount++;
+            }
+        }
+
+        if(instance.error){
+            if(instance.errorMessage === null){
+                instance.errorMessage = 'Unknown error occurred';
+            }
+            sendInjectCallback(false);
+        }
+        else if(canceled){
+            console.log('LCI [' + injectorName + ']: Injection Canceled. (' + loadedCount + '/' + _contentItems.length + ')');
+            sendInjectCallback(false);
+        }
+        else{
+            console.log('LCI [' + injectorName + ']: Injected. (' + loadedCount + '/' + _contentItems.length + ')');
+            sendInjectCallback(true);
         }
     }
 
@@ -71,43 +140,36 @@ function LooseContentInjector(injectorName, injectionInterval_ms) {
             if(ci.isCompleted()){ continue; }
 
 
-            if(ci.canLoadCheck()) {
+            if(ci.canInject()) {
                 ci.inject();
-                if (!ci.isLoaded()) {
+                if (!ci.isInjected()) {
                     ci.failures++;
-                    ci.lastError = "Injection check failed.";
+                    ci.lastError = "Injection check failed";
                 }
             }
             else{
                 ci.failures++;
-                ci.lastError = "Injection conditions not met.";
+                ci.lastError = "Injection conditions not met";
             }
 
             if(ci.isCompleted()){
 
-                var msg = null;
+                var preText = 'LCI [' + injectorName + '][' + ci.name + ']: ';
                 if(ci.isFailed()){
-                    msg = 'Injection failed. (' + ci.lastError + ')'
+                    console.log(preText + 'Injection failed. (' + ci.lastError + ')');
+                    if(ci.required){
+                        instance.error = true;
+                        instance.errorMessage = 'A Required injection failed.';
+                    }
                 }
                 else{
-                    msg = 'Injection success.';
+                    console.log(preText + 'Injection success. ');
                 }
-
-                console.log('LCI [' + injectorName + '][' + ci.name + ']: ' + msg);
-
                 sendContentCallback(ci);
             }
-
         }
-        if(isComplete()){
-            var loadedCount = 0;
-            for(var i = 0; i < _contentItems.length; i++){
-                if(_contentItems[i].isLoaded()){
-                    loadedCount++;
-                }
-            }
-            console.log('LCI [' + injectorName + ']: Injected. (' + loadedCount + '/' + _contentItems.length + ')');
-            sendInjectCallback();
+        if(instance.isComplete()){
+            sendCompleted();
         }
         else{
             setTimeout(injectLoop, injectionInterval_ms);
